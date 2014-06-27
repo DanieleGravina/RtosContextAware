9a0f6b81caa694d436d18544e0013a1568cb3c55
Fernando Faenza <pyuma@hotmail.it>
2014-06-27 10:46:04 +0200

    Add virtual class subscribe

---
+++ subscribe_light_aware.h
@@ -0,0 +1,98 @@
+/* 
+ * File:   subscribe.h
+ * Author: nando
+ *
+ * Created on May 30, 2014, 11:40 AM
+ */
+
+#ifndef SUBSCRIBE_H
+#define	SUBSCRIBE_H
+
+#include <vector>
+#include <list>
+#include "miosix/miosix.h"
+#include <pthread.h>
+
+using namespace std;
+using namespace miosix;
+
+typedef void (*function_pointer)();
+
+/*
+ * Trigger rules used to get which kind of transaction we had
+ * */
+
+class TRIGGER_rule{
+public:
+    enum rules
+    {
+        IN_OUT,
+        OUT_IN 
+    };
+private:
+    TRIGGER_rule();
+};
+
+class subscribe {
+public:
+    subscribe();
+    virtual ~subscribe();
+    
+    /**
+     Allows user to register own function to an change event
+     @param trigger rules
+     @param pointer to function
+     @return status of register*/
+
+    bool record(TRIGGER_rule::rules x, function_pointer);
+    
+    /**
+     Deallocate a subscription
+     @param pointer to function
+     @return true if the element is found and deleted otherwise false*/
+    bool unrecord(function_pointer);
+    
+    /**
+     Called by light_aware manager when occurred a transaction of environment
+     @param toggle variable*/
+    void change (bool y);
+    
+    void setState(bool x);
+    bool getState();
+   
+    std::list<function_pointer> getClientsINOUT();
+    std::list<function_pointer> getClientsOUTIN();
+
+    void setTrans(TRIGGER_rule::rules x);
+    
+    TRIGGER_rule::rules getTrans();
+    
+    void addFunction(function_pointer f);
+    void removeFunction(function_pointer f);
+    
+    
+    
+ 
+private:
+    bool state;
+    TRIGGER_rule::rules trans;
+    std::list<function_pointer> clients_INOUT;
+    std::list<function_pointer> clients_OUTIN; 
+    pthread_t t1;
+
+    
+    
+protected:
+    /**
+    */
+   void *handler();
+    
+   static void *handler_helper(void *context){
+       
+        return ((subscribe *)context)->handler();
+   }
+
+};
+
+#endif	/* SUBSCRIBE_H */
+
