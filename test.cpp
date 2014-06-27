d2060f4f17535f43b7d14269a3ecd5116d0957f6
DanieleGravina <daniele.gravina@mail.polimi.it>
2014-06-27 17:50:36 +0200

    added test file

--- test.cpp
+++ test.cpp
@@ -1,50 +1,27 @@
 #include "test.h"
 
 void foo(){
-    printf("in-out");
+    printf("in->out");
 }
 
 void foo2(){
-    printf("out-in");
+    printf("out->in");
 }
-
 
 int test()
 {
+    /*first choose the desired algorithm for signal processing*/
     FFTAlgorithm algorithm;
+    
+    /*second choose the subscription implementation for the context aware */
     subscribe_light_aware sub;
     
+    /*call the desired context aware implementation*/
     light_aware light_awareness(algorithm, ADCInit::NO_TIMER, sub);
     
+    /*register callback function with a trigger rule*/
     light_awareness.registration(TRIGGER_rule::IN_OUT, &foo);
     light_awareness.registration(TRIGGER_rule::OUT_IN, &foo2);
-    
-    light_awareness.unregistration(&foo2);
-    
-    led1::mode(Mode::OUTPUT);
-    led2::mode(Mode::OUTPUT);
-    
-    
-    for(;;){
-        
-        printf("light level: %g\n", light_awareness.getMeasure());
-        
-        if(light_awareness.isOutdoor()){
-            printf("outside\n");
-            led1::low();
-            led2::high();
-        }
-        else
-        {
-            printf("inside\n");
-            led1::high();
-            led2::low();
-        }
-        
-        usleep(25*100*512);
-     
-    }
-           
     
 }
 
--- test.h
+++ test.h
@@ -14,19 +14,15 @@
 #include <goertzel_algorithm.h>
 #include <complex.h>
 #include <miosix/_examples/context_aware/adc.h>
-#include "subscribe.h"
 #include "subscribe_light_aware.h"
 
 using namespace std;
 using namespace miosix;
 
-typedef Gpio<GPIOD_BASE,14> led1;
-typedef Gpio<GPIOD_BASE,15> led2;
-
-typedef Gpio<GPIOC_BASE,0> led3;
-
-typedef Gpio<GPIOB_BASE,0> adcIn;
-
+/**
+ * Test file to show how to use subscription with context aware
+ * @return 
+ */
 int test();
 
 
